e3b7ca37edbb2e2d83e6aff49932332341598504
nabi <nabi@scvsoft.net>
2022-09-30 14:26:40 +0900

    Update template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,14 @@
 import "styles/taiwindcss.css";
 import "styles/globals.css";
-import { useEffect } from "react";
-import getConfig from "next/config";
 import type { AppProps } from "next/app";
-import AlertContextProvider from "contexts/AlertContext";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { publicRuntimeConfig } = getConfig();
-
-  useEffect(() => {
-    console.log(`work on: ${publicRuntimeConfig.VERCEL_ENV}`);
-    console.log(`git commit: ${publicRuntimeConfig.COMMIT_SHA}`);
-  }, [publicRuntimeConfig.COMMIT_SHA, publicRuntimeConfig.VERCEL_ENV]);
-
   return (
-    <AlertContextProvider>
-      {/* @ts-ignore */}
+    <>
       <Component {...pageProps} />
-    </AlertContextProvider>
+      <ToastContainer position="top-center" pauseOnFocusLoss={false} />
+    </>
   );
 }
 
