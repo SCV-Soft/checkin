1b394cef236c1b39df7de9d4f6f1efb0ebdd3ca4
ritajeong <ritajeong.dev@gmail.com>
2022-10-05 14:43:38 +0900

    Seperate QR generator page from home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,10 @@
 import type { GetServerSideProps, NextPage } from "next";
-import QRCode from "react-qr-code";
 
 const Home: NextPage = () => {
-  const value = "Hello SCVSoft!";
   return (
-    <div className="container px-4 py-8 mx-auto">
-      <h1 className="text-xl">QR Generator</h1>
-      <h2>
-        Next QR code has text - <span className="text-blue-500">"{value}"</span>
-      </h2>
-      <div className="p-4 bg-white">
-        <QRCode value={value} />
-      </div>
-      <p>
-        reference :{" "}
-        <a href="https://www.npmjs.com/package/react-qr-code" className="hover:text-blue-500 hover:underline">
-          https://www.npmjs.com/package/react-qr-code
-        </a>
-      </p>
+    <div className={"container mx-auto px-4 py-8"}>
+      <h1>Hello SCVSoft!</h1>
+      <p>This is just template project</p>
     </div>
   );
 };
