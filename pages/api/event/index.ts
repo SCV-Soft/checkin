ff5a5ba6b6730f8967ccbb74531e7bce9548cf19
nabi <nabi@scvsoft.net>
2022-10-18 15:21:46 +0900

    Add event list api

---
+++ pages/api/event/index.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { database } from "utils/prisma";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case "GET": {
+      try {
+        const event = await database.event.findMany({
+          select: {
+            id: true,
+            name: true,
+            description: true,
+          },
+        });
+
+        return res.status(200).json(event);
+      } catch (e) {
+        console.error(e);
+        if (e instanceof Error) return res.status(500).end(e.message);
+        return res.status(500).end("Internal Server Error");
+      }
+    }
+    default: {
+      return res.status(405).end();
+    }
+  }
+}
