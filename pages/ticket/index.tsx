02feb42c7f9d3d2c91d65e258243e3dd15e92f2f
ritajeong <ritajeong@scvsoft.net>
2022-10-18 15:41:16 +0900

    Put QR generator into ticket page

--- pages/ticket/index.tsx
+++ pages/ticket/index.tsx
@@ -1,8 +1,12 @@
 import { NextPage } from "next";
+import QRCode from "react-qr-code";
 
 const Ticket: NextPage = () => {
   // TODO GET EVENT INFO FROM URL QUERY
   const title: string = "행사 이름";
+
+  const QRvalue = "Hello SCVSoft!";
+
   return (
     <div className="container mx-auto flex h-screen w-4/5 flex-col items-center px-4 py-8 lg:w-3/4">
       <div className="hero">
@@ -12,11 +16,12 @@ const Ticket: NextPage = () => {
           </div>
         </div>
       </div>
-      {
-        // TODO LINK REMOTE QR CODE IMAGE OR BASE64
-      }
       <div className="card w-96 bg-white shadow-xl lg:w-1/2">
-        <figure className="h-96 bg-primary"></figure>
+        <figure className="h-96 bg-primary">
+          <div className="bg-white p-4">
+            <QRCode value={QRvalue} />
+          </div>
+        </figure>
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
           <p>스태프에게 위 입장권을 제시하세요.</p>
